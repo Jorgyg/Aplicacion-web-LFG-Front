dfe0bbf5548afe8b55253dcecae98c06ec8b1375
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-07-27 18:44:38 +0200

    cambio rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { PerfilComponent } from './complements/perfil/perfil.component';
 import { AjustesGrupoComponent } from './complements/ajustes-grupo/ajustes-grupo.component';
 
 const routes: Routes = [
+  { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: 'register', component: RegisterComponent },
