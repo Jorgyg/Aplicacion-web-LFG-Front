8c3701c19b3d0ee098ec9f3692ddbfc82abf18ee
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-07-27 18:30:53 +0200

    componentes

---
+++ src/app/complements/chat-grupo/chat-grupo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-grupo',
+  templateUrl: './chat-grupo.component.html',
+  styleUrls: ['./chat-grupo.component.css']
+})
+export class ChatGrupoComponent {
+
+}
---
+++ src/app/complements/crear-grupo/crear-grupo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-grupo',
+  templateUrl: './crear-grupo.component.html',
+  styleUrls: ['./crear-grupo.component.css']
+})
+export class CrearGrupoComponent {
+
+}
