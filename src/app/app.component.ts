b66b64a6c5175d3e079f4c3592c89e7468878498
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-08-11 19:36:25 +0200

    cambio color botones sgallego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,4 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 
 export class AppComponent {
-  constructor(private router: Router) {
-    this.changeBodyColorBasedOnRoute(this.router.url);
-
-    // Suscribirse a los eventos de cambio de ruta
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.changeBodyColorBasedOnRoute(event.url);
-      }
-    });
-  }
-
-  private changeBodyColorBasedOnRoute(route: string) {
-    // Aquí defines las rutas y colores correspondientes para cada componente
-    const routeColorMap: { [key: string]: string } = {
-      '/login': '#D8F3DC', // Cambia '/login' con la ruta real de tu LoginComponent
-      '/register': '#D8F3DC', // Cambia '/register' con la ruta real de tu RegisterComponent
-      '/home': '#1B4332', // Cambia '/home' con la ruta real de tu HomeComponent
-      '/crear': '#1B4332', // Cambia '/crear' con la ruta real de tu HomeComponent
-      '/': '#1B4332',
-      '/perfil': '#1B4332',
-      '/main': '#1B4332',
-      '/chat': '#1B4332',
-      '/logros': '#1B4332', 
-      '/ajustes': '#1B4332',
-      '/unirse': '#1B4332',
-      '/eventos': '#1B4332', 
-      '/miembros': '#1B4332',
-      '/retar': '#1B4332',  // Cambia '/home' con la ruta real de tu HomeComponent
-      // Agrega más rutas y colores según tus necesidades
-    };
-
-    const bodyColor = routeColorMap[route] || '#ffffff';
-
-    // Cambiar el color del body
-    document.body.style.backgroundColor = bodyColor;
-  }
 }
