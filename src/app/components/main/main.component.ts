3c04bf864af8b96b975fa961e37e9358f551e172
Jorgyg <jgraslorenzo@gmail.com>
2023-08-17 12:44:04 +0200

    Primeras animaciones

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Group {
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-main',
@@ -6,5 +12,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  selectedCardIndex: number | null = null;
+  groups: Group[] = [
+    { title: 'Grupo 1', description: 'Descripción del grupo 1' },
+    { title: 'Grupo 2', description: 'Descripción del grupo 2' },
+    { title: 'Grupo 3', description: 'Descripción del grupo 3' },
+    { title: 'Grupo 1', description: 'Descripción del grupo 1' },
+    { title: 'Grupo 2', description: 'Descripción del grupo 2' },
+    { title: 'Grupo 3', description: 'Descripción del grupo 3' },
+    { title: 'Grupo 1', description: 'Descripción del grupo 1' },
+    { title: 'Grupo 2', description: 'Descripción del grupo 2' },
+    { title: 'Grupo 3', description: 'Descripción del grupo 3' },
+    // Agrega más grupos según sea necesario
+  ];
+  firstLinkActivated = false;
+  
+  constructor(private renderer: Renderer2, private router: Router) {}
 
+  enlargeAndNavigate(index: number) {
+    if (!this.firstLinkActivated) {
+      this.firstLinkActivated = true;
+    }
+
+
+    const cardElements = document.querySelectorAll('.card');
+
+    cardElements.forEach((cardElement: Element, i: number) => {
+      if (i === index) {
+        this.renderer.addClass(cardElement, 'enlarged-card');
+      } else {
+        this.renderer.removeClass(cardElement, 'enlarged-card');
+      }
+    });
+
+    this.selectedCardIndex = index;
+
+    setTimeout(() => {
+      this.router.navigate(['/chat']);
+    }, 1000); // Redirige después de 0.3 segundos (300 ms)
+  }
 }
