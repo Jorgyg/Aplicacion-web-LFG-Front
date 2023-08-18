e6f3b863b22fc287c9f203b208f8609797457126
Jorgyg <jgraslorenzo@gmail.com>
2023-08-18 15:06:30 +0200

    Mas animaciones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isEnlarged: boolean = false;
 
+  constructor(private renderer: Renderer2, private router: Router) {}
+
+  enlargeAndNavigate(route: string) {
+    this.isEnlarged = true;
+
+    setTimeout(() => {
+      this.router.navigate([route]);
+    }, 1000); // Ajusta el tiempo según tus necesidades
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
