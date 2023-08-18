d19e993c7fee7dbd858442aac0716602fddbb06d
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-08-18 12:01:26 +0200

    creacion de constante para cambiar de bg segun la ruta sgallego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,48 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  //Creamos una variable para guardar el background según la url que recibe el programa
+  backgroundUrl!: string;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void{
+    // Al iniciar la ruta, guarda el evento que pertenece a la url de la ruta en la que estemos
+    this.router.events
+    .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      //Llama al metodo setBackgroundImage donde le pasamos por parámetro la url de la ruta actual
+      this.cambiarFondo(event.url);
+    });
+  }
+
+  private cambiarFondo(url: string): void {
+    //Creamos una constante con cada ruta de la app y el background que le pertenece
+    const bg: { [key: string]: string } = {
+      '/': '/assets/img/bg-1.png',
+      '/home': '/assets/img/bg-1.png',
+      '/about': '/assets/img/bg-1.jpg',
+      "login":'',
+      "register":'',
+      "main":'',
+      "crear":'',
+      "unirse":'',
+      "chat": '',
+      "logros":'',
+      "miembros": '',
+      "eventos":'',
+      "ajustes":'',
+      "perfil":'',
+      "retar":''
+    };
+    //Comprueba que la url recibida se encuentra en la constante de rutas y le asigna el background que le pertenece
+    if (bg[url]) {
+      this.backgroundUrl = `url(${bg[url]})`;
+    } else {
+      // En caso de no existir se estblecerá un fondo por defecto
+      this.backgroundUrl = '/assets/img/background-about.jpg'; // o establecer algún otro valor predeterminado
+    }
+  }  
+
 }
