d5881c0f374f781b7c61190588a6c06f9d919e83
Jorgyg <jgraslorenzo@gmail.com>
2023-09-19 15:15:06 +0200

    Arreglado unirse

--- src/app/components/grupo/unirse-grupo/unirse-grupo.component.ts
+++ src/app/components/grupo/unirse-grupo/unirse-grupo.component.ts
@@ -23,30 +23,41 @@ export class UnirseGrupoComponent {
   maxParticipantes: any;
   cantidadUsers: any;
   grupoLleno: boolean = false;
-
+  grupoNoExiste = false;
   onSubmit(): void {
     const { codgrupo } = this.form;
     const user = this.userService.getUser();
     const username = user.infoUser.username;
 
-    this.groupService.getGrupo(codgrupo).subscribe((data) => {
-      this.maxParticipantes = data.participantes;
-    });
-    this.groupService.getUsuariosGrupo(codgrupo).subscribe((data) => {
-      this.cantidadUsers = data.length;
-    });
-    setTimeout(() => {
-      if (this.maxParticipantes > this.cantidadUsers) {
-        this.groupService.postUsuarioGrupo(codgrupo, username, false).subscribe(
-          (data) => {},
-          (err) => {
-            console.log(err);
+    this.groupService.getGrupo(codgrupo).subscribe(
+      (data) => {
+        this.maxParticipantes = data.participantes;
+        console.log(this.maxParticipantes);
+        this.groupService.getUsuariosGrupo(codgrupo).subscribe((data) => {
+          this.cantidadUsers = data.length;
+        });
+        setTimeout(() => {
+          if (this.maxParticipantes > this.cantidadUsers) {
+            this.groupService.postUsuarioGrupo(codgrupo, username, false).subscribe(
+              (data) => {},
+              (err) => {
+                console.log(err);
+              }
+            );
+          } else {
+            this.grupoLleno = true;
           }
-        );
-        this.router.navigate(['/chat', codgrupo]);
-      } else {
-        this.grupoLleno = true;
-      }
-    }, 1000);
+        }, 1000);
+      },
+      (error) => {
+        this.grupoNoExiste = true;
+        setTimeout(() =>{
+        this.grupoNoExiste = false;
+
+        },3000);
+      });
+
+        
+   
   }
 }
