8297c1852c02e7c421ef763fedfb6897bde453e2
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-09-19 17:42:30 +0200

    Update unirse-grupo.component.ts

--- src/app/components/grupo/unirse-grupo/unirse-grupo.component.ts
+++ src/app/components/grupo/unirse-grupo/unirse-grupo.component.ts
@@ -42,11 +42,16 @@ export class UnirseGrupoComponent {
         setTimeout(() => {
           if (this.maxParticipantes > this.cantidadUsers) {
             this.groupService.postUsuarioGrupo(codgrupo, username, false).subscribe(
-              (data) => {},
+              (data) => {
+
+              },
               (err) => {
                 console.log(err);
               }
+
             );
+            this.router.navigate(['/chat', codgrupo]);
+
           } else {
             this.grupoLleno = true;
           }
@@ -59,6 +64,7 @@ export class UnirseGrupoComponent {
 
         },3000);
       });
+
 
   }
 }
